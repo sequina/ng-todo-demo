4708aaebfb40159532e06e99a597aa7eeb8f6fda
sequina <sccaro89@gmail.com>
2016-05-18 21:19:22 -0500

    navbar with ctlrs

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
 var app = angular.module("AwesomeApp", []);
 
+app.controller("NavCtrl", function($scope) {
+  $scope.navItems = [{name: "Logout"},{name: "All Items"}, {name: "New Item"} ]
+})
+
+
 app.controller("TodoCtrl", function ($scope) {
   $scope.welcome = "hello";
 
