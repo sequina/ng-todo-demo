f541fdeabf3138446c77fe4680bff994a320f16f
sequina <sccaro89@gmail.com>
2016-05-21 09:18:04 -0500

    fixed nodes

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 var app = angular.module("AwesomeApp", []);
 
 app.controller("NavCtrl", function($scope) {
-  $scope.navItems = [{name: "Logout"},{name: "All Items"}, {name: "New Item"} ]
-})
+  $scope.navItems = [{name: "Logout"},{name: "All Items"}, {name: "New Item"} ];
+});
 
 
 app.controller("TodoCtrl", function ($scope) {
