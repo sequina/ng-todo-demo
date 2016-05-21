129761b94d350c03d70a82509eb1f44aa784039b
sequina <sccaro89@gmail.com>
2016-05-21 13:28:30 -0500

    routes implemented

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("AwesomeApp", []);
+var app = angular.module("AwesomeApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider.
+  when('/items/list',{
+    templateUrl:'partials/item-list.html',
+    controller:"TodoCtrl"
+  }).
+  when('/items/new', {
+    templateUrl: 'partials/item-new.html',
+    controller:"TodoCtrl"
+  }).
+  when('/items/details', {
+    templateUrl: 'partials/item-details.html',
+    controller:"TodoCtrl"
+  }).
+  otherwise('/items/list');
+});
